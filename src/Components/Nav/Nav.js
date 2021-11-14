fd47e86fca11ae0d8a1029ce581f73fa049cd472
myriahnottage <myriah.nottage@gmail.com>
2021-11-14 03:39:01 -0800

    navbar basics, still needs work on toggle

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import '../main.css';
+import logo from '../../img/seahagcafe.png';
+
+export default function Nav() {
+
+    const [isActive, setActive] = useState("false");
+
+    const toggleMenuSelector = () => {
+        setActive(!isActive);
+    }
+
+    return (
+        <div className="header">
+        <label htmlFor="navToggle" className={isActive ? "navToggleLabel change" : "navToggleLabel"} onClick={toggleMenuSelector}>
+        <span className="bar1"></span>
+        <span className="bar2"></span>
+        <span className="bar3"></span>
+      </label>
+      <a className="logoAnchor" href="/"><img src={logo} alt="Sea Hag Cafe Logo" href="/" className="logo" /></a>
+      <input type="checkbox" id="navToggle" className="navToggle" />
+      <nav>
+        <ul>
+          <li><a href="/blog" onClick={toggleMenuSelector}>Blog</a></li>
+          <li><a href="/projects">Projects</a></li>
+        </ul>
+      </nav>
+    </div>
+    )
+};
